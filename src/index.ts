f01e32af03380a6a82dd3cb26d477f1da8082963
cabralpinto <jmcabralpinto@gmail.com>
2025-10-04 16:12:00 +0100

    Modify info.languageServiceHost in place

--- src/index.ts
+++ src/index.ts
@@ -6,31 +6,39 @@ import YAML from 'yaml';
 export = ({ typescript: ts_ }: { typescript: typeof ts }) => ({
   create: (info: ts.server.PluginCreateInfo) => {
     const logger = info.project.projectService.logger;
-    const languageServiceHost = {
-      getScriptKind: filename => {
-        if (!info.languageServiceHost.getScriptKind) return ts_.ScriptKind.Unknown;
-        if (/\.ya?ml$/.test(filename)) return ts_.ScriptKind.TS;
-        return info.languageServiceHost.getScriptKind(filename);
-      },
-      getScriptSnapshot: filename => {
-        if (!/\.ya?ml$/.test(filename))
-          return info.languageServiceHost.getScriptSnapshot(filename);
-        const content = fs.readFileSync(filename, 'utf8');
-        let object;
-        try {
-          object = YAML.parse(content);
-        } catch (error) {
-          logger.info(`[typescript-plugin-yaml] YAML.parse error:\n${error}`);
-        }
-        const text = `export default ${JSON.stringify(object)};`;
-        return ts_.ScriptSnapshot.fromString(text);
-      },
-      resolveModuleNameLiterals: (moduleLiterals, containingFile, ...rest) =>
-        info.languageServiceHost.resolveModuleNameLiterals!(
-          moduleLiterals,
-          containingFile,
-          ...rest
-        ).map((resolvedModule, index) => {
+    const languageServiceHost = info.languageServiceHost;
+
+    const getScriptKind = languageServiceHost.getScriptKind?.bind(languageServiceHost);
+    languageServiceHost.getScriptKind = filename => {
+      if (!getScriptKind) return ts_.ScriptKind.Unknown;
+      if (/\.ya?ml$/.test(filename)) return ts_.ScriptKind.TS;
+      return getScriptKind(filename);
+    };
+
+    const getScriptSnapshot =
+      languageServiceHost.getScriptSnapshot.bind(languageServiceHost);
+    languageServiceHost.getScriptSnapshot = filename => {
+      if (!/\.ya?ml$/.test(filename)) return getScriptSnapshot(filename);
+      const content = fs.readFileSync(filename, 'utf8');
+      let object;
+      try {
+        object = YAML.parse(content);
+      } catch (error) {
+        logger.info(`[typescript-plugin-yaml] YAML.parse error:\n${error}`);
+      }
+      const text = `export default ${JSON.stringify(object)};`;
+      return ts_.ScriptSnapshot.fromString(text);
+    };
+
+    const resolveModuleNameLiterals =
+      languageServiceHost.resolveModuleNameLiterals!.bind(languageServiceHost);
+    languageServiceHost.resolveModuleNameLiterals = (
+      moduleLiterals,
+      containingFile,
+      ...rest
+    ) =>
+      resolveModuleNameLiterals(moduleLiterals, containingFile, ...rest).map(
+        (resolvedModule, index) => {
           const moduleName = moduleLiterals[index].text;
           if (!/\.ya?ml$/.test(moduleName)) return resolvedModule;
           return {
@@ -41,12 +49,9 @@ export = ({ typescript: ts_ }: { typescript: typeof ts }) => ({
               resolvedFileName: path.resolve(path.dirname(containingFile), moduleName)
             }
           };
-        })
-    } as Partial<ts.LanguageServiceHost>;
-    const languageServiceHostProxy = new Proxy(info.languageServiceHost, {
-      get: (target, key: keyof ts.LanguageServiceHost) =>
-        languageServiceHost[key] ? languageServiceHost[key] : target[key]
-    });
-    return ts_.createLanguageService(languageServiceHostProxy);
+        }
+      );
+
+    return info.languageService;
   }
 });
