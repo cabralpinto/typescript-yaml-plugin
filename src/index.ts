fa615fa2bc506190a0c262e963d7e006fbedfaa5
cabralpinto <jmcabralpinto@gmail.com>
2025-10-05 09:17:01 +0100

    Include YAML files in import path autocomplete

--- src/index.ts
+++ src/index.ts
@@ -6,20 +6,19 @@ import YAML from 'yaml';
 export = ({ typescript: ts_ }: { typescript: typeof ts }) => ({
   create: (info: ts.server.PluginCreateInfo) => {
     const logger = info.project.projectService.logger;
-    const languageServiceHost = info.languageServiceHost;
+    const { languageServiceHost, languageService } = info;
 
     const getScriptKind = languageServiceHost.getScriptKind?.bind(languageServiceHost);
-    languageServiceHost.getScriptKind = filename => {
+    languageServiceHost.getScriptKind = fileName => {
       if (!getScriptKind) return ts_.ScriptKind.Unknown;
-      if (/\.ya?ml$/.test(filename)) return ts_.ScriptKind.TS;
-      return getScriptKind(filename);
+      if (/\.ya?ml$/.test(fileName)) return ts_.ScriptKind.TS;
+      return getScriptKind(fileName);
     };
-
     const getScriptSnapshot =
       languageServiceHost.getScriptSnapshot.bind(languageServiceHost);
-    languageServiceHost.getScriptSnapshot = filename => {
-      if (!/\.ya?ml$/.test(filename)) return getScriptSnapshot(filename);
-      const content = fs.readFileSync(filename, 'utf8');
+    languageServiceHost.getScriptSnapshot = fileName => {
+      if (!/\.ya?ml$/.test(fileName)) return getScriptSnapshot(fileName);
+      const content = fs.readFileSync(fileName, 'utf8');
       let object;
       try {
         object = YAML.parse(content);
@@ -29,7 +28,6 @@ export = ({ typescript: ts_ }: { typescript: typeof ts }) => ({
       const text = `export default ${JSON.stringify(object)};`;
       return ts_.ScriptSnapshot.fromString(text);
     };
-
     const resolveModuleNameLiterals =
       languageServiceHost.resolveModuleNameLiterals!.bind(languageServiceHost);
     languageServiceHost.resolveModuleNameLiterals = (
@@ -52,6 +50,41 @@ export = ({ typescript: ts_ }: { typescript: typeof ts }) => ({
         }
       );
 
-    return info.languageService;
+    const languageServiceOverride = {
+      getCompletionsAtPosition(fileName, position, options, formattingSettings) {
+        const completions = languageService.getCompletionsAtPosition(
+          fileName,
+          position,
+          options,
+          formattingSettings
+        );
+        if (!completions) return completions;
+        let importPath = languageService
+          .getProgram()
+          ?.getSourceFile(fileName)
+          ?.getFullText()
+          .slice(0, position)
+          .match(/(?<=(?:import(?:\(|\s?)|require\(|from\s?)["']).+?$/s)?.[0];
+        if (!importPath) return completions;
+        if (importPath.startsWith('.'))
+          importPath = path.resolve(path.dirname(fileName), importPath);
+        fs.globSync(`${importPath}*.{yaml,yml}`)
+          .map(fileName => path.basename(fileName))
+          .forEach(baseFileName =>
+            completions.entries.push({
+              name: baseFileName,
+              kind: ts_.ScriptElementKind.scriptElement,
+              kindModifiers: '.yaml',
+              sortText: '11'
+            })
+          );
+        return completions;
+      }
+    } as Partial<ts.LanguageService>;
+    const languageServiceProxy = new Proxy(languageService, {
+      get: (target, key: keyof ts.LanguageService) =>
+        languageServiceOverride[key] ?? target[key]
+    });
+    return languageServiceProxy;
   }
 });
